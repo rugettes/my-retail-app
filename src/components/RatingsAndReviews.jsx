bb447ea5c593582a1370eed9e408a3099cc0ec67
Rachel <rlense88@gmail.com>
2018-12-26 22:36:42 -0600

    update ratings and reviews
    replace "for" with appropriate attribute

--- src/components/RatingsAndReviews.jsx
+++ src/components/RatingsAndReviews.jsx
@@ -7,34 +7,34 @@ export default class RatingsAndReviews extends Component {
       <div className="ratings-and-reviews col-md-12 col-lg-6">
         <div className="ratings">
           <fieldset className="stars">
-            <label className="full" for="star5" title="5 stars"></label>
+            <label className="full" htmlFor="star5" title="5 stars"></label>
             <input type="radio" id="star5" name="rating" value="5" />
 
-            <label className="half" for="star4half" title="4.5 stars"></label>
+            <label className="half" htmlFor="star4half" title="4.5 stars"></label>
             <input type="radio" id="star4half" name="rating" value="4 and a half" />
 
-            <label className="full" for="star4" title="4 stars"></label>
+            <label className="full" htmlFor="star4" title="4 stars"></label>
             <input type="radio" id="star4" name="rating" value="4" />
 
-            <label className="half" for="star3half" title="3.5 stars"></label>
+            <label className="half" htmlFor="star3half" title="3.5 stars"></label>
             <input type="radio" id="star3half" name="rating" value="3 and a half" />
 
-            <label className="full" for="star3" title="3 stars"></label>
+            <label className="full" htmlFor="star3" title="3 stars"></label>
             <input type="radio" id="star3" name="rating" value="3" />
 
-            <label className="half" for="star2half" title="2.5 stars"></label>
+            <label className="half" htmlFor="star2half" title="2.5 stars"></label>
             <input type="radio" id="star2half" name="rating" value="2 and a half" />
 
-            <label className="full" for="star2" title="2 stars"></label>
+            <label className="full" htmlFor="star2" title="2 stars"></label>
             <input type="radio" id="star2" name="rating" value="2" />
 
-            <label className="half" for="star1half" title="1.5 stars"></label>
+            <label className="half" htmlFor="star1half" title="1.5 stars"></label>
             <input type="radio" id="star1half" name="rating" value="1 and a half" />
 
-            <label className="full" for="star1" title="1 star"></label>
+            <label className="full" htmlFor="star1" title="1 star"></label>
             <input type="radio" id="star1" name="rating" value="1" />
 
-            <label className="half" for="starhalf" title="0.5 stars"></label>
+            <label className="half" htmlFor="starhalf" title="0.5 stars"></label>
             <input type="radio" id="starhalf" name="rating" value="half" />
           </fieldset>
           <p className="text-lowercase">overall</p>
