a2798a31bb28260c6790af043c4be1e4927fdef7
Rachel <rlense88@gmail.com>
2018-12-26 22:16:15 -0600

    update carousel with icons and todos

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,9 +7,15 @@ export default class Carousel extends Component {
     document.querySelector('.carousel .primary').setAttribute('src', clickedImageSrc)
   }
 
+  onMagnifyingClick (e) {
+    // TODO: render modal with current primary image source
+  }
+
   render() {
     const primaryImageLocation = new RegExp(this.props.imageData.PrimaryImage[0].image)
     const alternateImageLocations = this.props.imageData.AlternateImages
+
+    // TODO: keep track of index cycling through array on arrow click; only render current, n+1, n-1
     let renderedImages = []
 
     for (let i = 0; i < 3; i++) {
@@ -19,10 +25,13 @@ export default class Carousel extends Component {
     return (
       <div className="carousel">
         <img src={primaryImageLocation.source} alt="" className="primary"></img>
+        <span className="view-larger"><i className="fas fa-search-plus"></i> View larger</span>
         <div className="text-center">
+          <i className="fas fa-angle-left arrows"></i>
           {renderedImages.map(location => (
             <img onClick={this.onImageClick}  src={location.image} alt="" className="rounded thumbnail"></img>
           ))}
+          <i className="fas fa-angle-right arrows"></i>
         </div>
       </div>
     )
