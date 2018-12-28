626aabd0abc86029b1a70712ff04242b30494b49
Rachel <rlense88@gmail.com>
2018-12-28 10:54:21 -0600

    Update QuantityPicker.jsx
    incorporate state instead of manipulating innerHTML directly

--- src/components/QuantityPicker.jsx
+++ src/components/QuantityPicker.jsx
@@ -2,25 +2,33 @@ import React, { Component } from 'react'
 import './QuantityPicker.css'
 
 export default class QuantityPicker extends Component {
-  onPlusClick (e) {
-    const el = document.querySelector('.quantity-picker .quantity')
-    el.innerHTML++
+  constructor(props) {
+    super(props)
+    this.state = { quantity: this.props.quantity }
+    this.onPlusClick = this.onPlusClick.bind(this)
+    this.onMinusClick = this.onMinusClick.bind(this)
   }
-
-  onMinusClick (e) {
-    const el = document.querySelector('.quantity-picker .quantity')
-    el.innerHTML--
-  }
-
 
   render() {
     return (
       <div className="quantity-picker col-xs-12 col-md-5 col-lg-7">
         <label>quantity:</label>
         <i onClick={this.onPlusClick} className="fas fa-plus-circle"></i>
-        <div className="quantity">{this.props.quantity}</div>
+        <div className="quantity">{this.state.quantity}</div>
         <i onClick={this.onMinusClick} className="fas fa-minus-circle"></i>
       </div>
     )
+  }
+
+  onPlusClick() {
+    this.setState(state => ({
+      quantity: parseInt(state.quantity) + 1
+    }))
+  }
+
+  onMinusClick() {
+    this.setState(state => ({
+      quantity: parseInt(state.quantity) - 1
+    }))
   }
 }
