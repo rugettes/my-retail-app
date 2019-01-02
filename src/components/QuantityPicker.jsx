f7824283c941f9a8f23c4d7c27b369f950f9164c
Rachel Lense <rlense88@gmail.com>
2019-01-02 14:47:39 -0600

    update quantity picker
    change div to number input
    add handleChange function
    hide number spinner

--- src/components/QuantityPicker.jsx
+++ src/components/QuantityPicker.jsx
@@ -5,13 +5,15 @@ export default class QuantityPicker extends Component {
   constructor(props) {
     super(props)
     this.state = { quantity: this.props.quantity }
-    this.onPlusClick = this.onPlusClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     this.onMinusClick = this.onMinusClick.bind(this)
+    this.onPlusClick = this.onPlusClick.bind(this)
   }
 
-  onPlusClick() {
+  handleChange(e) {
+    const { value } = e.target
     this.setState(state => ({
-      quantity: parseInt(state.quantity) + 1
+       quantity: value
     }))
   }
 
@@ -21,12 +23,18 @@ export default class QuantityPicker extends Component {
     }))
   }
 
+  onPlusClick() {
+    this.setState(state => ({
+      quantity: parseInt(state.quantity) + 1
+    }))
+  }
+
   render() {
     return (
       <div className="quantity-picker col-xs-12 col-md-5 col-lg-7">
         <label>quantity:</label>
         <i onClick={this.onPlusClick} className="fas fa-plus-circle"></i>
-        <div className="quantity">{this.state.quantity}</div>
+        <input className="quantity" value={this.state.quantity} onChange={this.handleChange} type="number"></input>
         <i onClick={this.onMinusClick} className="fas fa-minus-circle"></i>
       </div>
     )
